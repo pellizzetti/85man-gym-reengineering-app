4cdf97730ded460e9019251246bda197bf88ca72
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2019-04-12 11:50:18 -0300

    Init persons api endpoint fetching

--- src/pages/persons/list/index.js
+++ src/pages/persons/list/index.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { Box } from 'grommet';
 
-const PersonsList = ({ match }) => (
-  <Box flex align="center" justify="center">
-    <h3>
-      ID:
-      {match.params.personType}
-    </h3>
-  </Box>
-);
+import api from '~/services/api';
 
-PersonsList.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape(),
-  }).isRequired,
-};
+class PersonsList extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        personType: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    results: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results },
+      } = await api.get('/persons');
+
+      console.log(results);
+
+      this.setState({
+        results,
+        loading: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading, results } = this.state;
+    return (
+      <Box flex align="center" justify="center">
+        <h3>
+          ID:
+          {match.params.personType}
+          <ul>
+            {loading ? <div>Carregando</div> : results.map(ele => <li key={ele.id}>{ele.name}</li>)}
+          </ul>
+        </h3>
+      </Box>
+    );
+  }
+}
 
 export default withRouter(PersonsList);
