3b6c2d34943c11b491b36a069b99e363a42e59a7
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2019-05-25 22:02:45 -0300

    Handle post and put in API service

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,11 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+api.postOrPut = (url, id, data, config = {}) => {
+  const method = id ? 'put' : 'post';
+  const apiUrl = id ? `${url}/${id}` : url;
+
+  return api[method](apiUrl, data, config);
+};
+
 export default api;
