15964a13047e3da82aa4c80db697e928e68c307b
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2019-06-08 17:45:36 -0300

    Ajust dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Box } from 'grommet';
+import { Box, Heading } from 'grommet';
+
+import Logo from './styles';
+
+import logoImage from '~/assets/images/logo.png';
 
 const Dashboard = () => (
-  <Box flex align="center" justify="center">
-    Dashboard
+  <Box flex align="center" justify="center" direction="row">
+    <Logo src={logoImage} alt="Logo da GMV" />
+    <Heading level="2" color="brand">
+      Dashboard
+    </Heading>
   </Box>
 );
 
---
+++ src/pages/dashboard/styles.js
@@ -0,0 +1,8 @@
+﻿import styled from 'styled-components';
+
+const Logo = styled.img`
+  width: 55px;
+  margin-right: 10px;
+`;
+
+export default Logo;
